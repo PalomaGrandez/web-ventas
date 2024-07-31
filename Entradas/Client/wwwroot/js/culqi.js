4855cb933461d00ef52aacef874236c334a2afd5
devtestapps <devtestapps062024@gmail.com>
2024-07-31 10:28:51 -0500

    cambios

--- Entradas/Client/wwwroot/js/culqi.js
+++ Entradas/Client/wwwroot/js/culqi.js
@@ -1,16 +1,8 @@
 const settings = {
-    title: 'Realizacion de pago',
+    title: 'Culqi  store 2',
     currency: 'PEN',
-    amount: 600, // Asegúrate de que el monto está en centavos
-};
-
-//c2391c29-3b71-4a27-b55e-bac3457ecb1f
-/* -----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDSGBntzxDV2WecsK5MicVaXb3J
-sz1QFAIO6GYkcsQGic4aug1zYESr06sCzSCPXQT1N1P + uss8CgasPeNtmE + LHm + q
-HvvqMv6 + yjzWeNon4QiqbOHvRu4ZwHhNok53c3abnNQ + OufTwcbQv7puOkvAhyIq
-OXgb2n / A79IFmX63mwIDAQAB
------ END PUBLIC KEY-----*/
+    amount: 8000,
+}
 
 const paymentMethods = {
     tarjeta: false,
@@ -31,7 +23,7 @@ const options = {
 };
 
 const client = {
-    email: 'mario.correa@fabrica.pe',
+    email: 'soporte@fabrica.pe',
 };
 
 const appearance = {
@@ -41,7 +33,6 @@ const appearance = {
     hiddenBanner: false,
     hiddenToolBarAmount: false,
     menuType: "sidebar",
-    buttonCardPayText: "Pagar 6.00 PEN",
     logo: null,
     defaultStyle: {
         bannerColor: "blue",
@@ -53,6 +44,43 @@ const appearance = {
     },
 };
 
+
+const handleCulqiAction = async () => {
+    if (Culqi.token) {
+        const token = Culqi.token.id;
+        console.log('Se ha creado un Token: ', token);
+
+        const response = await fetch('https://localhost:7122/api/pagos/procesar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                TokenId: token
+            }),
+        });
+
+        const result = await response.json();
+        console.log('Respuesta del backend: ', result);
+
+        if (response.ok) {
+            alert('Pago procesado exitosamente.');
+        } else {
+            alert('Error al procesar el pago: ' + result.message);
+        }
+    } else if (Culqi.order) {
+        const order = Culqi.order;
+        console.log('Se ha creado el objeto Order: ', order);
+    } else {
+        console.log('Errorrr : ', Culqi.error);
+    }
+}
+
+window.AbrirCulqi = function (settings) {
+
+    Culqi.open();
+};
+
 const config = {
     settings,
     client,
@@ -60,60 +88,8 @@ const config = {
     appearance,
 };
 
-const publicKey = 'pk_live_f88f5d1d2457d6af'; 
+const publicKey = 'pk_test_3153d0c7ed6a853a'; 
 
 const Culqi = new CulqiCheckout(publicKey, config);
-
-window.initCulqi = function (buttonId) {
-    const btn_pagar = document.getElementById(buttonId);
-    if (btn_pagar) {
-        btn_pagar.addEventListener('click', function (e) {
-            Culqi.open();
-            e.preventDefault();
-        });
-    }
-};
-
-window.AbrirCulqi = function () {
-    Culqi.open();
-};
-
-const handleCulqiAction = async () => {
-    if (Culqi.token) {
-        const token = Culqi.token.id;
-        console.log('Se ha creado un Token: ', token);
-
-        try {
-            const response = await fetch('https://localhost:7122/api/pagos/procesar', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    TokenId: token
-                }),
-            });
-
-            const result = await response.json();
-            console.log('Respuesta del backend: ', result);
-
-            if (response.ok) {
-                alert('Pago procesado exitosamente.');
-            } else {
-                alert('Error al procesar el pago: ' + result.message);
-            }
-        } catch (error) {
-            console.error('Error en la solicitud de pago:', error);
-            alert('Error en la solicitud de pago.');
-        }
-
-        Culqi.close();
-    } else if (Culqi.order) {
-        const order = Culqi.order;
-        console.log('Se ha creado el objeto Order: ', order);
-    } else {
-        console.log('Error : ', Culqi.error);
-    }
-};
 
 Culqi.culqi = handleCulqiAction;
--- Entradas/Server/Controllers/PagoController.cs
+++ Entradas/Server/Controllers/PagoController.cs
@@ -13,18 +13,13 @@ namespace Entradas.Server.Controllers
         {
             // Log para verificar que el token se recibe correctamente
             Console.WriteLine($"Token recibido: {request.TokenId}");
-
-            if (string.IsNullOrEmpty(request.TokenId))
-            {
-                return BadRequest(new { message = "El TokenId es requerido" });
-            }
-
+        
             // Crear una instancia del cliente RestSharp
             var client = new RestClient("https://api.culqi.com/v2/charges");
 
             // Crear una solicitud POST
             var restRequest = new RestRequest();
-            restRequest.AddHeader("Authorization", "Bearer sk_live_6d709dfb06d8fea5"); // Usa tu llave secreta de prueba
+            restRequest.AddHeader("Authorization", "Bearer sk_test_47b1d0312b63a309"); // Usa tu llave secreta de prueba
             restRequest.AddHeader("Content-Type", "application/json");
 
             // Configurar el método HTTP como POST
@@ -33,13 +28,13 @@ namespace Entradas.Server.Controllers
             // Crear el payload usando solo el token recibido
             var payload = new
             {
-                amount = 600, // Monto en céntimos, ajusta según sea necesario
+                amount = request.Amount, // Monto en céntimos, ajusta según sea necesario
                 currency_code = "PEN",
                 source_id = request.TokenId, // El token recibido del frontend
                 capture = true,
                 description = "Pago realizado con Yape", // Descripción del pago
-                email = "mario.correa@fabrica.pe",
-                
+                email = "soporte@fabrica.pe",
+                    
             };
 
             // Agregar el cuerpo de la solicitud
@@ -65,5 +60,7 @@ namespace Entradas.Server.Controllers
     public class TokenRequest
     {
         public string TokenId { get; set; }
+
+        public int Amount { get; set; }
     }
 }
